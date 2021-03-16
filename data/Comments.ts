d4fda48961695becef1461a37b6ddf3e55522729
sanminpark <80050268+sanminpark@users.noreply.github.com>
2021-03-16 23:16:21 +0900

    Update Comments.ts

--- data/Comments.ts
+++ data/Comments.ts
@@ -6,39 +6,104 @@ export type Comment = {
   id: string; // make this a unique number starting with 1, ex. "101"
   post: string; // ID of the post it belongs to, ex. "001"
   user: string; // username of person commenting
-  op: boolean // true if it's from the original poster, false otherwise
+  op: boolean // true if it"s from the original poster, false otherwise
   text: string; // body of comment
   timestamp: string; // like Twitter, ex. "31s", "5m", "7h", "2d", "Feb 21", "Mar 20, 2010"
   upvotes: number;
   downvotes: number;
   upvoted: boolean; // defaults to false
   downvoted: boolean; // defaults to false
-  hasImage: boolean; // true if there's an image attached, false otherwise
+  hasImage: boolean; // true if there"s an image attached, false otherwise
   image: Image; // image attachment
   topLevel: boolean; // false if this is a reply to another comment, true otherwise
   replies: string[]; // array of IDs of replies to this comment in chronological order;
-                     // should be EMPTY if this itself is a reply, because we're only doing 1 layer of nesting
+                     // should be EMPTY if this itself is a reply, because we"re only doing 1 layer of nesting
 }
 */
 
-export const comments = [
-    { // feel free to edit/delete this example, it's just for reference
-        id: '100',
-        post: '000',
-        user: 'nifty_salamander',
+export const Comments = [
+    { // finalized
+        id: "101",
+        post: "001",
+        user: "izipizi",
         op: false,
-        text: 'This is an example comment!',
-        timestamp: '2h',
+        text: "wheres the light source supposed to be? i feel like the shadows on her shoulders and sleeves r coming from different angles...",
+        timestamp: "Feb 25",
+        upvotes: 0,
+        downvotes: 0,
+        upvoted: false,
+        downvoted: false,
+        hasImage: false,
+        image: null,
+        topLevel: true,
+        replies: [],
+    },
+
+    { // finalized
+        id: "103",
+        post: "002",
+        user: "Saturno_22",
+        op: false,
+        text: "I think the right eye is a little bit off (her right). She’s turning her head, but that eye looks like it’s facing forward.",
+        timestamp: "Feb 19",
+        upvotes: 0,
+        downvotes: 0,
+        upvoted: false,
+        downvoted: false,
+        hasImage: false,
+        image: null,
+        topLevel: true,
+        replies: ["104", "105", "106"],
+    },
+    
+    { // finalized
+        id: "104",
+        post: "002",
+        user: "izipizi",
+        op: false,
+        text: "hmmm i think @Saturno_22 is right. it should be distorted to account for the angle. maybe try something like this?",
+        timestamp: "Feb 20",
         upvotes: 0,
         downvotes: 0,
         upvoted: false,
         downvoted: false,
         hasImage: true,
-        image: require("../assets/images/togepi.jpeg"),
-        topLevel: true,
+        image: "../assets/images/animegirl-redlined.png",
+        topLevel: false,
         replies: [],
     },
-    {
-        // more comments
+
+    { // finalized
+        id: "105",
+        post: "002",
+        user: "Saturno_22",
+        op: false,
+        text: "Yea that looks a lot better imo!!",
+        timestamp: "Feb 21",
+        upvotes: 0,
+        downvotes: 0,
+        upvoted: false,
+        downvoted: false,
+        hasImage: false,
+        image: null,
+        topLevel: false,
+        replies: [],
+    },
+
+    { // finalized 
+        id: "106",
+        post: "002",
+        user: "CityOwls",
+        op: true,
+        text: "Omg that was it! I adjusted the eye and the face isn’t bothering me anymore haha. Thank you!!",
+        timestamp: "Feb 21",
+        upvotes: 0,
+        downvotes: 0,
+        upvoted: false,
+        downvoted: false,
+        hasImage: false,
+        image: null,
+        topLevel: false,
+        replies: [],
     },
 ];
